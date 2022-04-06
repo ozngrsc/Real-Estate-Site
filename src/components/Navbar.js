2afee520ecf7e507a624e41e7bc045078e981e74
ozngrsc <ozangursucu@hotmail.com>
2022-04-06 13:13:01 +0300

    second commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,69 @@
+import React from "react";
+import styled, { css } from "styled-components/macro";
+import { Link } from "react-router-dom";
+import { menuData } from "../data/MenuData";
+import { Button } from "./Button";
+
+const Nav = styled.nav`
+  height: 60px;
+  display: flex;
+  justify-content: space-between;
+  padding: 1rem 2rem;
+  z-index: 100;
+  position: fixed;
+  width: 100%;
+  background: red;
+`;
+
+const NavLink = css`
+  color: #fff;
+  display: flex;
+  align-items: center;
+  padding: 0 1rem;
+  height: 100%;
+  cursor: pointer;
+  text-decoration: none;
+`;
+
+const Logo = styled(Link)`
+  ${NavLink}
+  font-style: italic;
+`;
+
+const MenuBars = styled.i``;
+
+const NavMenu = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const NavMenuLinks = styled(Link)`
+  ${NavLink}
+`;
+
+const NavBtn = styled.div`
+  display: flex;
+  align-items: center;
+  margin-right: 24px;
+`;
+
+const Navbar = () => {
+  return (
+    <Nav>
+      <Logo to="/">ELIXR</Logo>
+      <MenuBars />
+      <NavMenu>
+        {menuData.map((item, index) => (
+          <NavMenuLinks to={item.link} key={index}>
+            {item.title}
+          </NavMenuLinks>
+        ))}
+      </NavMenu>
+      <NavBtn>
+        <Button to="/contact">Contact Us</Button>
+      </NavBtn>
+    </Nav>
+  );
+};
+
+export default Navbar;
