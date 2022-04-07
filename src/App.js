5d54ddb86d837e6bdcd8396252c75960803e2085
ozngrsc <ozangursucu@hotmail.com>
2022-04-07 13:00:09 +0300

    slider data

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import GlobalStyle from "./globalStyles";
 import Hero from "./components/Hero";
+import { SliderData } from "./data/SliderData";
 
 function App() {
   return (
     <>
       <GlobalStyle />
       <Navbar />
-      <Hero />
+      <Hero slides={SliderData} />
     </>
   );
 }
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,5 +1,8 @@
 import React from "react";
 import styled, { css } from "styled-components/macro";
+import { Button } from "./Button";
+import { IoMdArrowRoundForward } from "react-icons/io";
+import { IoArrowForward, IoArrowBack } from "react-icons/io5";
 
 const HeroSection = styled.section`
   height: 100vh;
@@ -18,11 +21,76 @@ const HeroWrapper = styled.div`
   position: relative;
 `;
 
-const Hero = () => {
+const HeroSlide = styled.div``;
+const HeroSlider = styled.div``;
+const HeroImage = styled.img``;
+const HeroContent = styled.div``;
+const Arrow = styled(IoMdArrowRoundForward)``;
+
+const SliderButtons = styled.div`
+  position: absolute;
+  bottom: 50px;
+  right: 50px;
+  display: flex;
+  z-index: 10;
+`;
+
+const arrowButtons = css`
+  width: 50px;
+  height: 50px;
+  color: #fff;
+  cursor: pointer;
+  background: #000d1a;
+  border-radius: 50px;
+  padding: 10px;
+  margin-right: 1rem;
+  user-select: none;
+  transition: 0.3s;
+
+  &:hover {
+    background: #cd853f;
+    transform: scale() (1.05);
+  }
+`;
+
+const PrevArrow = styled(IoArrowBack)`
+  ${arrowButtons}
+`;
+const NextArrow = styled(IoArrowForward)`
+  ${arrowButtons}
+`;
+
+const Hero = ({ slides }) => {
   return (
     <HeroSection>
       <HeroWrapper>
-        <h1>Heroo</h1>
+        {slides.map((slide, index) => {
+          return (
+            <HeroSlide key={index}>
+              <HeroSlider>
+                <HeroImage />
+                <HeroContent>
+                  <h1>{slide.title}</h1>
+                  <p>{slide.price}</p>
+                  <Button
+                    to={slide.path}
+                    primary="true"
+                    css={`
+                      max-width: 160px;
+                    `}
+                  >
+                    {slide.label}
+                    <Arrow />
+                  </Button>
+                </HeroContent>
+              </HeroSlider>
+            </HeroSlide>
+          );
+        })}
+        <SliderButtons>
+          <PrevArrow />
+          <NextArrow />
+        </SliderButtons>
       </HeroWrapper>
     </HeroSection>
   );
